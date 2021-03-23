06bb3b02c5c1098d1799fa07e973010a92bd9b23
Basile Savouret <basile@smartb.city>
2021-03-23 13:19:40 +0100

    request util function

---
+++ web/packages/utils/src/http/index.ts
@@ -0,0 +1,44 @@
+
+export interface httpOptions {
+    url: string,
+    method: "GET" | "PUT" | "POST" | "DELETE",
+    body?: string,
+    jwt?: string,
+    contentType?: "application/json" | "text/plain" | "none"
+    returnType?: "json" | "text"
+    errorHandler?: (error: Error) => void
+}
+
+export const request = <T>(
+    options: httpOptions
+): Promise<T> => {
+    const { method, url, body, contentType = "application/json", jwt, errorHandler, returnType = "json" } = options
+    return fetch(url, {
+        method: method,
+        headers: {
+            ...(jwt !== undefined && jwt !== ""
+                ? {
+                    Authorization: `Bearer ${jwt}`,
+                }
+                : {}),
+            ...(contentType !== "none"
+                ? {
+                    "Content-Type": contentType,
+                }
+                : {}),
+        },
+        body: body,
+    })
+        .then((response) => {
+            if (!response.ok) {
+                response.text().then((error) => {
+                    throw new Error(error);
+                })
+            }
+            if (returnType === "json") {
+                return response.json()
+            } 
+            return response.text()
+        })
+        .catch(errorHandler);
+};
