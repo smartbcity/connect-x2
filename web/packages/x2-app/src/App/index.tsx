a02757ea935ba98891cc4c879830c1129fb61193
Basile Savouret <basile@smartb.city>
2021-03-26 16:53:27 +0100

    menu is now able to display the current page

--- web/packages/x2-app/src/App/index.tsx
+++ web/packages/x2-app/src/App/index.tsx
@@ -1,31 +1,32 @@
 import { Menu, MenuItem } from "@smartb/archetypes-ui-layout";
 import { AppLayout } from "components";
 import { useEffect, useMemo } from "react";
-import {useTranslation, TFunction } from "react-i18next";
-import { LinkProps, Link } from "react-router-dom";
+import { useTranslation, TFunction } from "react-i18next";
+import { LinkProps, Link, useLocation } from "react-router-dom";
 import { SSM } from "ssm";
 import { AppRouter } from "./routes";
 import withConnect from "./withConnect";
 
 interface AppProps {
-    title: string
-    ssmList: Map<string, SSM>
+  title: string
+  ssmList: Map<string, SSM>
 }
 
 const App = (props: AppProps) => {
-    const { title, ssmList } = props
-    const {t} = useTranslation()
-    const menu = useMenu(t, ssmList)
+  const { title, ssmList } = props
+  const { t } = useTranslation()
+  const location = useLocation()
+  const menu = useMenu(t, ssmList, location.pathname)
 
-    useEffect(() => {
-        if (title !== "X2") {
-            document.title = `${title} - X2`
-        }
-    }, [title])
+  useEffect(() => {
+    if (title !== "X2") {
+      document.title = `${title} - X2`
+    }
+  }, [title])
 
   return (
     <AppLayout title={title} menu={menu}>
-        <AppRouter/>
+      <AppRouter />
     </AppLayout>
   );
 }
@@ -33,31 +34,35 @@ const App = (props: AppProps) => {
 export default withConnect(App);
 
 
-const useMenu = (t: TFunction, ssmList: Map<string, SSM>) => {
-    return useMemo(() => getMenu(t, ssmList), [ssmList])
-  }
-  
-  const getMenu = (t: TFunction, ssmList: Map<string, SSM>): Menu<LinkProps>[] => {
-    const protocolsList: MenuItem<LinkProps>[] = Array.from(ssmList.values()).map((ssm) => ({
-      key: `appLayout-protocols-${ssm.name}`,
-      label: ssm.name,
-      component: Link,
-      componentProps: {
-        to: `/${ssm.name}/sessions`
-      }
-    }))
-    const menu: Menu<LinkProps>[] = [{
-      key: "appLayout-dashboard",
-      label: t("dashboard"),
-      component: Link,
-      componentProps: {
-        to: '/'
-      }
+const useMenu = (t: TFunction, ssmList: Map<string, SSM>, path: string) => {
+  return useMemo(() => getMenu(t, ssmList, path), [ssmList, path])
+}
+
+const getMenu = (t: TFunction, ssmList: Map<string, SSM>, path: string): Menu<LinkProps>[] => {
+  const ssmName = path.split("/")[1]
+  const protocolsList: MenuItem<LinkProps>[] = Array.from(ssmList.values()).map((ssm) => ({
+    key: `appLayout-protocols-${ssm.name}`,
+    label: ssm.name,
+    component: Link,
+    componentProps: {
+      to: `/${ssm.name}/sessions`
     },
-    {
-      key: "appLayout-protocols",
-      label: t("protocols"),
-      items: protocolsList
-    }]
-    return menu
-  }
+    isSelected: ssm.name === ssmName
+  }))
+  const menu: Menu<LinkProps>[] = [{
+    key: "appLayout-dashboard",
+    label: t("dashboard"),
+    component: Link,
+    componentProps: {
+      to: '/'
+    },
+    isSelected: path === "/"
+  },
+  {
+    key: "appLayout-protocols",
+    label: t("protocols") + ":",
+    items: protocolsList,
+    isSelected: ssmName.trim() !== "" && ssmName !== undefined
+  }]
+  return menu
+}
