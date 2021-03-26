8992e571b40d86b26cfac38aea89ec3d2486537c
Basile Savouret <basile@smartb.city>
2021-03-26 16:56:46 +0100

    details card displaying json

--- web/packages/x2-app/src/App/pages/Details/components/DetailsCard.tsx
+++ web/packages/x2-app/src/App/pages/Details/components/DetailsCard.tsx
@@ -1,11 +1,51 @@
-import {Panel} from "components"
+import { Panel } from "components"
+import { useMemo } from "react";
 import { useTranslation } from "react-i18next"
+//@ts-ignore
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+//@ts-ignore
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { SessionLog } from "ssm";
+import { highLevelStyles } from "utils";
+import stringifyObject from 'stringify-object'
 
-export const DetailsCard = () => {
-    const {t} = useTranslation()
+const useStyles = highLevelStyles({
+    panel: {
+        width: "40%"
+    },
+    highlighter: {
+        height: "100%",
+        margin: "0 !important"
+    },
+    body: {
+        height: "100%"
+    }
+})
+
+interface DetailsCardProps {
+    currentLog?: SessionLog
+}
+
+export const DetailsCard = (props: DetailsCardProps) => {
+    const { currentLog } = props
+    const { t } = useTranslation()
+    const classes = useStyles()
+
+    const highlighted = useMemo(() => {
+        if (!currentLog) return
+        return stringifyObject({
+            origin: currentLog.state.origin,
+            public: currentLog.state.public,
+        }, {
+            indent: '  ',
+            singleQuotes: false,
+        })
+    }, [currentLog])
     return (
-        <Panel header={t("detailsPage.itemDetails")}>
-            <div style={{height: "200px"}}/>
+        <Panel className={classes.panel} noPadding bodyClassName={classes.body} header={t("detailsPage.transactionDetails")}>
+            <SyntaxHighlighter className={classes.highlighter} language="json" style={tomorrow}>
+                {highlighted ?? ""}
+            </SyntaxHighlighter>
         </Panel>
     )
 }
