7c1822d3b8b18d26b7bb365a6c569fb0d7d75e5b
Basile Savouret <basile@smartb.city>
2021-04-30 12:56:30 +0200

    use codeHighlither

--- web/packages/x2-app/src/App/pages/Details/components/DetailsCard.tsx
+++ web/packages/x2-app/src/App/pages/Details/components/DetailsCard.tsx
@@ -1,21 +1,16 @@
+import { CodeHighlighter } from "@smartb/archetypes-ui-components";
 import { Panel } from "components"
-import { useMemo } from "react";
 import { useTranslation } from "react-i18next"
-//@ts-ignore
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-//@ts-ignore
-import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { SessionLog } from "ssm";
 import { highLevelStyles } from "utils";
-import stringifyObject from 'stringify-object'
 
 const useStyles = highLevelStyles({
     panel: {
-        width: "40%"
-    },
-    highlighter: {
-        height: "100%",
-        margin: "0 !important"
+        width: "40%",
+        "& pre": {
+            height: "100%",
+            margin: "0 !important"
+        }
     },
     body: {
         height: "100%"
@@ -31,21 +26,12 @@ export const DetailsCard = (props: DetailsCardProps) => {
     const { t } = useTranslation()
     const classes = useStyles()
 
-    const highlighted = useMemo(() => {
-        if (!currentLog) return
-        return stringifyObject({
-            origin: currentLog.state.origin,
-            public: currentLog.state.public,
-        }, {
-            indent: '  ',
-            singleQuotes: false,
-        })
-    }, [currentLog])
     return (
         <Panel className={classes.panel} noPadding bodyClassName={classes.body} header={t("detailsPage.transactionDetails")}>
-            <SyntaxHighlighter className={classes.highlighter} language="json" style={tomorrow}>
-                {highlighted ?? ""}
-            </SyntaxHighlighter>
+            {currentLog && <CodeHighlighter object={{
+                origin: currentLog.state.origin,
+                public: currentLog.state.public,
+            }} language="json" />}
         </Panel>
     )
 }
