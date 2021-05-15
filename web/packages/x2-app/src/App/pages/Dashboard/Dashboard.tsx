0e49671d7de2283dd542edcf213a768c3bd99c7a
Basile Savouret <basile@smartb.city>
2021-05-15 13:55:20 +0200

    dashboard implementation

--- web/packages/x2-app/src/App/pages/Dashboard/Dashboard.tsx
+++ web/packages/x2-app/src/App/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,9 @@
 import { Page } from "components";
 import { useTranslation } from "react-i18next";
-import { Typography } from "@material-ui/core";
+import { CardsHeader } from "./components/CardsHeader";
+import { Label, LabelSwitch } from "@smartb/archetypes-ui-components";
+import { useCallback, useMemo, useState } from "react";
+import { Box } from "@material-ui/core";
 
 interface DashboardProps {
     setTitle: (title: string) => void
@@ -9,12 +12,40 @@ interface DashboardProps {
 export const Dashboard = (props: DashboardProps) => {
   const {setTitle} = props;
   const {t} = useTranslation()
+  const [labelValue, setLabelValue] = useState("3Month")
+
+  const labels = useMemo((): Label[] => [{
+    name: "Month",
+    value: "Month",
+    key: "LabelSwitch_month"
+  },{
+    name: "3 months",
+    value: "3Month",
+    key: "LabelSwitch_3month"
+  },{
+    name: "Year",
+    value: "Year",
+    key: "LabelSwitch_year"
+  },{
+    name: "All",
+    value: "All",
+    key: "LabelSwitch_all"
+  }], [])
+
+  const onLabelChange = useCallback(
+    (value: string) => setLabelValue(value),
+    [],
+  )
+
   return (
   <Page
     setTitle={setTitle}
     title={t("dashboard")}
   >
-      <Typography>Je suis le dashboard</Typography>
+      <CardsHeader />
+      <Box display="flex" justifyContent="flex-end">
+        <LabelSwitch selectedLabelValue={labelValue} onLabelChange={onLabelChange} labels={labels} />
+      </Box>
   </Page>
   );
 };
