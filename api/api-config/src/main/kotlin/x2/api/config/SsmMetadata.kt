b51132fdf9d844649fa48b9d09d0c602535ed6f0
Luc Heydel <luc@smartb.city>
2021-08-25 10:34:59 +0200

    use new ssm autoconfig

--- api/api-config/src/main/kotlin/x2/api/config/SsmMetadata.kt
+++ api/api-config/src/main/kotlin/x2/api/config/SsmMetadata.kt
@@ -1,7 +1,8 @@
 package x2.api.config
 
+import ssm.tx.dsl.config.TxSsmLocationProperties
 import ssm.tx.dsl.features.query.SsmName
 import ssm.tx.dsl.model.SsmVersion
 
-typealias SsmMetadataMap = Map<SsmName, Map<SsmVersion, SsmLocationProperties>>
+typealias SsmMetadataMap = Map<SsmName, Map<SsmVersion, TxSsmLocationProperties>>
 
